4c5292a0f414cc5df41442d010aa944059b17ae0
L <eckeriaue@estelink.ru>
2023-11-27 21:05:52 +0300

    done

--- main.go
+++ main.go
@@ -4,16 +4,41 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
+
+type Film struct {
+	Title    string
+	Director string
+}
 
 func main() {
 
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.Execute(w, nil)
+		films := map[string][]Film{
+			"Films": {
+				{Title: "sdfsdf", Director: "DSfsdf"},
+				{Title: "sdfsdf", Director: "DSfsdf"},
+				{Title: "sdfsdf", Director: "DSfsdf"},
+				{Title: "sdfsdf", Director: "DSfsdf"},
+			},
+		}
+
+		tmpl.Execute(w, films)
+	}
+
+	h2 := func(w http.ResponseWriter, r *http.Request) {
+		title := r.PostFormValue("title")
+		director := r.PostFormValue("director")
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		duration := time.Second
+		time.Sleep(duration)
+		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
 	}
 
 	http.HandleFunc("/", h1)
+	http.HandleFunc("/add-film/", h2)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
